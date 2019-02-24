259fd3a865b82870a6f2f2ef65f3b3eb284d7180
Shane Eardley <eardley.shane@gmail.com>
2019-02-24 11:00:55 +0200

    Commit blank project structure

---
+++ Shane.Sender/Shane.Sender/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shane.Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
